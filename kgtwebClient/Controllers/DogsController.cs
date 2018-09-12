5468e90c0551d6b31ad6fb1271f0b7a7f28d7d97
Kasia <anxiety477@gmail.com>
2018-09-12 19:42:51 +0200

    WIP: delete and update dog buttons

--- kgtwebClient/Controllers/DogsController.cs
+++ kgtwebClient/Controllers/DogsController.cs
@@ -13,41 +13,112 @@ namespace kgtwebClient.Controllers
 {
     public class DogsController : Controller
     {
-        HttpClient client;
+
         //The URL of the WEB API Service
-        string url = "http://kgt.azurewebsites.net/api/employee/";
+        static string url = "http://kgt.azurewebsites.net/api/";
+        private static readonly HttpClient client = new HttpClient { BaseAddress = new Uri(url) };
 
 
         // GET: Employees
         public async Task<ActionResult> Index()
         {
-            client = new HttpClient();
-            client.BaseAddress = new Uri(url);
+            //client.BaseAddress = new Uri(url);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            HttpResponseMessage responseMessage = await client.GetAsync("dogs/");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var responseData = responseMessage.Content.ReadAsStringAsync().Result;
 
-                var Employees = JsonConvert.DeserializeObject<List<EmployeeInfo>>(responseData);
+                var dogs = JsonConvert.DeserializeObject<List<Dog>>(responseData);
 
                 //return View(Employees);
                 //ViewBag.Persons = data;
 
-                var employeesList = new ListOfEmployeeInfo
+                var dogsList = new DogsList
                 {
-                    listOfEmployee = Employees
+                    ListOfDogs = dogs
                 };
 
                 ViewBag.RawData = responseData;
                 //ViewBag.Employees = Employees;
 
 
-                return View(employeesList);
+                return View(dogsList);
             }
             return View();
+        }
+
+        public bool DeleteDog(int? id)
+        {
+            //client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            /* dla put i post:
+            httpmethod.put i httpmethod.post
+            message.Content = new StringContent(***object-json-serialized***, 
+                                                System.Text.Encoding.UTF8, "application/json");
+             */
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Delete, client.BaseAddress +"dogs/" + id.ToString());
+            message.Content = new StringContent(id.ToString(), System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage responseMessage = client.SendAsync(message).Result;
+            if (responseMessage.IsSuccessStatusCode)    //200 OK
+            {
+                //wyswietlić informację
+                message.Dispose();
+                return true;
+            }
+            else    // wiadomosc czego się nie udałos
+            {
+                message.Dispose();
+                return false;
+            }
+
+        }
+
+        public bool UpdateDog(int? id)
+        {
+            //client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            /* dla put(update) i post(add):
+            httpmethod.put i httpmethod.post
+            message.Content = new StringContent(***object-json-serialized***, 
+                                                System.Text.Encoding.UTF8, "application/json");
+             */
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Put, client.BaseAddress + "dogs/?id=" + id.ToString());
+            var dog = new Dog
+            {
+                DogID = 9,
+                Name = "Łapczor",
+                DateOfBirth = new DateTime(2010, 1, 1),
+                Level = 1,
+                Workmode = new List<Object>(),
+                Notes = "Notatki",
+                Guide = 1
+            };
+
+            var dogSerialized = JsonConvert.SerializeObject(dog);
+            
+
+            message.Content = new StringContent(dogSerialized /*id.ToString()*/, System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage responseMessage = client.SendAsync(message).Result;
+            if (responseMessage.IsSuccessStatusCode)    //200 OK
+            {
+                //wyswietlić informację
+                message.Dispose();
+                return true;
+            }
+            else    // wiadomosc czego się nie udałos
+            {
+                message.Dispose();
+                return false;
+            }
+
         }
     }
 }
---
+++ kgtwebClient/Models/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kgtwebClient.Models
+{
+    public class Dog
+    {
+        public int DogID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Object Level { get; set; }
+        public List<Object> Workmode { get; set; }
+        public string Notes { get; set; }
+        public Object Guide { get; set; }
+
+    }
+
+    public class DogsList
+    {
+        public List<Dog> ListOfDogs { get; set; }
+    }
+}
